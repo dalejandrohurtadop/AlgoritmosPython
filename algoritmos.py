3efdafc375e4be2f65fdd41166a94eb8fe5c5ffd
Alejandro Hurtado <dalejandrohurtadop@gmail.com>
2023-12-22 15:22:54 -0500

    Correccion avanza resolucion ejerciocio 4, pendiente mejorar y 14 completo

--- algoritmos.py
+++ algoritmos.py
@@ -1,3 +1,5 @@
+import time
+
 # 1. Escriba un programa en Python que imprima la siguiente secuencia del eneunciado correspondiente
 
 def secuencia_escalonada(n):
@@ -33,6 +35,115 @@ def secuencia_ordenada_escalonada(n):
                 print(j, end=" ")
                 j=j+1
         
+#4. Escriba un programa en python que lea un número entero y determine si es primo.
+                
+def es_primo01(n):
+    n_medios = int(round(n/2,0) + 1)
 
+    if n%2 == 0:
+        print("El numero ", n, " no es primo")
+    elif n ==3:
+        return True
+    else:
+        for i in range(3, n_medios+1, 2):
+            if(n%i == 0 ):
+                return print("El numero ", n, " no es primo"), print("Tiene como factor común el número  :", i) 
         
-secuencia_ordenada_escalonada(120)
+        return print("El numero ", n, " es primo")
+
+def es_primo02(n):
+    n_medios = int(round(n/2,0) + 1)
+
+    if (n == 3) | (n ==2):
+        return True
+    elif n%2 == 0 | n == 1:
+        return False
+    else:
+        for i in range(2, n_medios+1, 1):
+            if(n%i == 0 ):
+                return False 
+        return True
+    
+
+#14. Escriba un programa en Python que imprima los números primos del 1 al 100.
+    
+def lista_primos(n):
+    lista = [num for num in range(1,n+1) if es_primo02(num)]
+    return lista
+
+def es_primo03(n):
+    n_medios = int(round(n/2,0) + 1)
+
+    if (n%2 == 0) | (n == 1):
+        return False
+    elif (n ==3) | (n==2):
+        return True
+    elif n_medios<=100000:
+        lista = lista_primos(n_medios+1)
+        for i in lista:
+            if n%i == 0:
+                return False 
+        return True
+    else:
+        lista = lista_primos(100000+1) + list(range(100000,n_medios+1,2))
+        for i in lista:
+            if n%i == 0:
+                return False 
+        return True
+    
+def es_primo04(n):
+    n_medios = int(round(n/2,0) + 1)
+
+    if (n%2 == 0) | (n == 1):
+        return False
+    elif (n ==3) | (n==2):
+        return True
+    else:
+           
+        lista=[num5 for num5 in [num3 for num3 in [num2 for  num2 in range(2, n_medios+1) if num2%2!=0] if num3%3!=0] if num5%5!=0]
+        lista.insert(0,3)
+        lista.insert(1,5)
+        for i in lista:
+            if(n%i == 0 ):
+                return False 
+        return True
+    
+#5. Escriba un programa en Python que lea un número entero y determine si es perfecto.
+    
+def divisores(n):
+
+    divisores=[1]
+    lista = lista_primos(n)
+
+    for i in lista:
+        if n%i ==0:
+            divisores.append(i)
+
+    return divisores
+ 
+def es_num_perfecto(n):
+    
+    sum_div = sum(divisores(n))
+
+    if sum_div == n:
+        return print(n, "Es número perfecto")
+    
+    return print(n, "No es número perfecto")
+
+
+   
+inicio = time.time()
+
+n=28
+print(divisores(n))
+print(es_num_perfecto(n))
+
+
+time.sleep(1)
+# -------------
+
+fin = time.time()
+print(fin-inicio)
+
+     
+
