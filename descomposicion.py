d00cdcf2b2ce3456b136265214dc581f983fa853
Alejandro Hurtado <dalejandrohurtadop@gmail.com>
2023-12-30 00:21:14 -0500

    Adición archivo descomposicion.py ejercicio 3 curso udemy

---
+++ descomposicion.py
@@ -0,0 +1,15 @@
+while True:
+    numero = int(input("Ingrese un numero entero positivo: "))
+    if numero<=0:
+        continue
+    else:
+        break
+
+cadena = str(numero)
+digitos = len(cadena)
+dig = len(cadena) -1
+
+for cad in cadena:
+    num = int(cad)*(10**dig)
+    print(f"{num:0{digitos}d}")
+    dig -= 1
